5a45e6851455aa78c28abe3eb24b0f32a77dc6ec
chrism1 <chrism1@us.ibm.com>
2025-05-06 18:29:40 -0400

    updating modules

--- ibmcloud_base_agent/agent.py
+++ ibmcloud_base_agent/agent.py
@@ -1,12 +1,32 @@
 # ibmcloud_base_agent_a2a/agent.py
 from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
 
 AGENT_MODEL = "openai/gpt-4o-mini"
 
-root_agent = Agent(
-    name="ibmcloud_base_agent",
-    model=LiteLlm(model=AGENT_MODEL),
-    description="An IBM Cloud platform engineer",
-    instruction="You are an IBM Cloud platform engineer called Chris, you will act as a platform engineer with deep expertise in IBM Cloud service operations and patterns for cloud architecture. You have access to a set of tools that can be used to access resources in IBM Cloud accounts."
-)
+async def create_agent():
+    """Gets tools from MCP Server."""
+    tools, exit_stack = await MCPToolset.from_server(
+        connection_params=StdioServerParameters(
+            command='npx',
+            args=["-y",    # Arguments for the command
+                # Example MCP server command, using filesystem tool:
+                "@modelcontextprotocol/server-filesystem",
+                # TODO: IMPORTANT! Change the path below to an ABSOLUTE path on your system.
+                # TODO: Replace with env var or config.
+                "/Users/chrism1/Code/ai/agentic/a2a/ibmcloud-base-agent"
+            ],
+        )
+    )
+    agent = Agent(
+        name="ibmcloud_base_agent",
+        model=LiteLlm(model=AGENT_MODEL),
+        description="An IBM Cloud platform engineer",
+        # TODO: Replace with very specialized instructions for the IBM Cloud agent.
+        instruction="You are an IBM Cloud platform engineer called Chris, you will act as a platform engineer with deep expertise in IBM Cloud service operations and patterns for cloud architecture. You have access to a set of tools that can be used to access resources in IBM Cloud accounts.",
+        tools=tools
+    )
+    return agent,exit_stack
+
+root_agent = create_agent()
